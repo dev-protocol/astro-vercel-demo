a1268c216c2501f787700e52e7f310bd0839e55b
aggre <hiroyuki.aggre@gmail.com>
2022-12-23 02:29:03 +0000

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,5 @@
 import { config } from 'dotenv'
 import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import vue from '@astrojs/vue'
-import react from '@astrojs/react'
-import svelte from '@astrojs/svelte'
-import prefetch from '@astrojs/prefetch'
 import vercel from '@astrojs/vercel/serverless'
 
 config()
